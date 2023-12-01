c328c27b1fb30dd68786cf261489d831c3ac57d4
Aayushman Sharma <075bme004aayushman@gmail.com>
2023-12-01 23:10:58 +0545

    improvement in smoothness of carousel

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const container = document.getElementsByClassName("carousel-container");
 const imagesArray = Array.from(imageItems);
 const noOfImages = imageItems.length;
 console.log(noOfImages);
+const dotsArray = [];
 
 let counter = 0;
 
@@ -11,43 +12,85 @@ const leftBtn = document.getElementById("arrow-left");
 const rightBtn = document.getElementById("arrow-right");
 
 function directSlide(n){
-    imagesArray.forEach(image => image.style.transform = `translateX(-${(n)* 100}%)`);
+    imagesArray.forEach((image, index) => {
+        image.style.left = `${(index - n)*100}%`;
+        counter = n;
+    })
+
+    //imagesArray.forEach(image => image.style.transform = `translateX(-${(n)* 100}%)`);
+}
+const slideImageLeft = () => {
+    // imagesArray.forEach( (image, index) => {
+    //     image.style.transform = `translateX(-${counter* 100}%)`;
+    // });
+    imagesArray.forEach( (image, index) => {
+        let startPosition = 0;
+        let instantPosition = 0;
+        let intervalId = setInterval(() => {
+            if(counter === 5){
+                startPosition +=4
+                if(startPosition >= 400){
+                    clearInterval(intervalId);
+                }else{
+                    image.style.left = `${(index - counter + 1)*100 + startPosition + 4}%`;
+                }
+            }else{
+                instantPosition += 2;
+                if(instantPosition >= 100){
+                    clearInterval(intervalId);
+                }else{
+                    image.style.left = `${(index - counter + 1)*100 - instantPosition - 2}%`;
+                }
+            }
+            
+        }, 1000/60)
+    })
 }
 
-const slideImage = () => {
-    imagesArray.forEach( (image, index) => {
-        image.style.transform = `translateX(-${counter* 100}%)`;
-    });
+const slideImageRight = () => {
     // imagesArray.forEach( (image, index) => {
-    //     let currentPosition = 0;
-    //     let intervalId = setInterval(() => {
-    //         currentPosition += 0.05;
-    //         console.log(currentPosition);
-    //         if(currentPosition >= 100){
-    //             clearInterval(intervalId);
-    //         }else{
-    //             image.style.left = `${(index - counter)*100 - currentPosition}%`;
-    //         }
-    //     }, 1000/60)
-    // })
+    //     image.style.transform = `translateX(-${counter* 100}%)`;
+    // });
+    imagesArray.forEach( (image, index) => {
+        let endPosition = 0;
+        let instantPosition = 0;
+        let intervalId = setInterval(() => {
+            if(counter === 4){
+                endPosition += 4;
+                if(endPosition >= 400){
+                    clearInterval(intervalId);
+                }else{
+                    image.style.left = `${(index - counter + 4)*100 - endPosition - 4}%`;
+                }
+            }else{
+                instantPosition += 2;
+                if(instantPosition >= 100){
+                    clearInterval(intervalId);
+                }else{
+                    image.style.left = `${(index - counter - 1)*100 + instantPosition + 2}%`;
+                }
+            }
+        }, 1000/60)
+    })
 }
 
 const goPrev = () => {
+
     if(counter > 0){
         counter--;
     }else{
-        counter = noOfImages - 1;
+        counter = 4;
     }
-    slideImage()
+    slideImageRight()
 }
 
 const goNxt = () => {
-    if(counter < noOfImages - 1){
+    slideImageLeft()
+    if(counter < noOfImages){
         counter++;
     }else{
-        counter = 0;
+        counter = 1;
     }
-    slideImage()
 }
 
 const dotsContainer = document.createElement('div');
@@ -56,12 +99,12 @@ const dotsContainer = document.createElement('div');
     for (let i = 0; i < noOfImages; i++) {
       const dot = document.createElement('span');
       dot.className = 'dot';
-      dot.setAttribute('onclick', 'directSlide(' + i + ')');
+      dot.setAttribute('onclick', `directSlide(${i})`);
       dotsContainer.appendChild(dot);
+      dotsArray.push(dot);
 }
 
 document.body.appendChild(dotsContainer);
-
 
 leftBtn.addEventListener("click", goPrev);
 rightBtn.addEventListener("click", goNxt);
